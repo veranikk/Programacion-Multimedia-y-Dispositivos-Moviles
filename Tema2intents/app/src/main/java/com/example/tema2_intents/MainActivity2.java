d87f4f7adb5df0f2315da3fb8b1d3ec44b1eeb1e
Vera Martín <veramc@gmail.com>
2025-10-01 20:46:34 +0200

    Nuevos proyectos

---
+++ Tema2intents/app/src/main/java/com/example/tema2_intents/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.tema2_intents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i("Ejemplo2", "Estoy en on Start de la actividad 2");
+        Intent ejemplo= new Intent(Intent.ACTION_VIEW);
+        ejemplo.setData(Uri.parse("http:www.google.es"));
+        startActivity(ejemplo);
+    }
+}

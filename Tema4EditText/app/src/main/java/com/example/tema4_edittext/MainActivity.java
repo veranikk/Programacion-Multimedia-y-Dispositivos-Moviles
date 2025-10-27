a02d82d9673e11533201aacca2e6e40950cb4db1
Vera Martín <veramc@gmail.com>
2025-10-27 20:47:27 +0100

    Nuevo Tema

---
+++ Tema4EditText/app/src/main/java/com/example/tema4_edittext/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tema4_edittext;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.linear);
+
+        String[] opciones ={"Opción 1","Opción 2","Opción 3","Opción 4","Opción 5"};
+
+        AutoCompleteTextView textoLeido= findViewById(R.id.miTexto);
+
+        ArrayAdapter<String> adaptador= new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, opciones);
+
+        textoLeido.setAdapter(adaptador);
+    }
+}
